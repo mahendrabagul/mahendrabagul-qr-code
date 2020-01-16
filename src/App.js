2c88e4c995e8c3cfbab1b7ae062f23791386e40d
Mahendra Bagul <mahendra@infracloud.com>
2020-01-16 11:54:25 +0530

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Container, NavbarBrand,Navbar,  Row, Col } from 'reactstrap';
 import './App.css';
 import Demo from './Demo';
 function App() {
-    return < div className = "App" >
-        <Demo/>
-        </div>;
+    return <>
+            <Navbar color="light" light expand="md">
+              <NavbarBrand href="/">Mahendra Bagul</NavbarBrand>
+            </Navbar>
+            <Container >
+              <Row>
+                <Col  sm="12" md={{ size: 6, offset: 4 }}><Demo/></Col>
+              </Row>
+            </Container>
+          </>
 }
 
 export default App;
